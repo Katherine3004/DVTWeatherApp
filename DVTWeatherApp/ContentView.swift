fbc5a5cb8d5e17d8999f0e1e2539b38af6c3b6a0
Katherine <katherine.e.chambers7@gmail.com>
2024-06-13 19:18:03 +0200

    Initial Commit

---
+++ DVTWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DVTWeatherApp
+//
+//  Created by Katherine Chambers on 2024/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DVTWeatherApp/DVTWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVTWeatherAppApp.swift
+//  DVTWeatherApp
+//
+//  Created by Katherine Chambers on 2024/06/13.
+//
+
+import SwiftUI
+
+@main
+struct DVTWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
