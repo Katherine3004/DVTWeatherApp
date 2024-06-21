2ac59d8b8627bd0f0cda7d4c200618209bb96800
Katherine <katherine.e.chambers7@gmail.com>
2024-06-21 09:26:05 +0200

    Adds firebase and firebase functions

---
+++ DVTWeatherApp/Firebase/FirebaseViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  FirebaseViewModel.swift
+//  DVTWeatherApp
+//
+//  Created by Katherine Chambers on 2024/06/21.
+//
+
+import Foundation
+import Firebase
+import Combine
+import FirebaseDatabase
+
+class FirebaseViewModel: ObservableObject {
+    
+    @Published var locations: [Location] = []
+    
+    private var ref: DatabaseReference = Database.database().reference()
+    
+    init() {
+        fetchLocations()
+    }
+    
+    func fetchLocations() {
+        ref.child("locations").observeSingleEvent(of: .value) { snapShot in
+            var newLocations: [Location] = []
+            for child in snapShot.children {
+                if let snapShot = child as? DataSnapshot,
+                   let value = snapShot.value as? [String: Any],
+                   let name = value["name"] as? String,
+                   let lat = value["lat"] as? Double,
+                   let long = value["long"] as? Double {
+                    let location = Location(name: name, lat: lat, long: long)
+                }
+            }
+            self.locations = newLocations
+        }
+    }
+    
+    func addLocation(name: String, lat: Double, long: Double) {
+        let key = ref.child("locations").childByAutoId().key
+        let newLocation = ["name": name, "lat": lat, "long": long] as [String: Any]
+        ref.child("locations").child(key ?? UUID().uuidString).setValue(newLocation) { error, _ in
+            if let error = error {
+                print("DEBGUG Error addLocation(): \(error.localizedDescription) ")
+            }
+            else {
+                self.locations.append(Location(name: name, lat: lat, long: long))
+            }
+        }
+    }
+    
+    func deleteLocation(name: String) {
+        ref.child("locations").queryOrdered(byChild: "name").queryEqual(toValue: name).observeSingleEvent(of: .value) { snapShot in
+            for child in snapShot.children {
+                if let childSnapShot = child as? DataSnapshot {
+                    childSnapShot.ref.removeValue { error, _ in
+                        if let error = error {
+                            print("DEBUG Error deleteLocation(): \(error.localizedDescription)")
+                        }
+                        else {
+                            self.locations.removeAll { $0.name == name }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DVTWeatherApp/Firebase/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  DVTWeatherApp
+//
+//  Created by Katherine Chambers on 2024/06/21.
+//
+
+import Foundation
+
+struct Location {
+    let name: String
+    let lat: Double
+    let long: Double
+}
